38729342abf31294092b1aa348c65ae03a30506e
Gabriel Alves <bielvipbh@hotmail.com>
2021-05-31 21:53:01 -0300

    navigate to finances if user is logged

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AuthSelectors } from 'src/app/store/services/selectors';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent implements OnInit, OnDestroy {
+  private authSubscription: Subscription;
 
-  ngOnInit(): void {}
+  constructor(private authSelectors: AuthSelectors, private router: Router) {}
+
+  ngOnInit(): void {
+    this.authSubscription = this.authSelectors.state$.subscribe(authState => {
+      if (authState.done && authState.user) {
+        this.router.navigateByUrl('/finances');
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.authSubscription) {
+      this.authSubscription.unsubscribe();
+    }
+  }
 }
