8784806b135ea40ad7fcebecba94eebdc6caf890
Gabriel Alves <bielvipbh@hotmail.com>
2021-06-07 22:06:11 -0300

    add tailwind
    angular material doesn't have a grid, really? ._.'

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+const { guessProductionMode } = require('@ngneat/tailwind');
+
+process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
+
+module.exports = {
+  prefix: '',
+  important: true,
+  mode: 'jit',
+  purge: {
+    content: ['./src/**/*.{html,ts,css,scss,sass,less,styl}'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '600px',
+      md: '960px',
+      lg: '1280px',
+      xl: '1920px',
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
