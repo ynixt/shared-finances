dfa7b8937d2ea943fcb5b1c68ed47a179dde0658
Gabriel Alves <bielvipbh@hotmail.com>
2021-06-16 02:07:49 -0300

    title for categories

--- frontend/src/app/pages/shared-finances/shared-category/shared-category-routing.module.ts
+++ frontend/src/app/pages/shared-finances/shared-category/shared-category-routing.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditCategoryComponent, NewCategoryComponent } from 'src/app/components/category';
+import { TitleGuard } from 'src/app/shared';
 import { SharedCategoryComponent } from './shared-category.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SharedCategoryComponent,
+    canActivate: [TitleGuard],
+    data: {
+      title: 'categories',
+    },
   },
   {
     path: 'new',
     component: NewCategoryComponent,
+    canActivate: [TitleGuard],
+    data: {
+      title: 'new-category',
+    },
   },
   {
     path: ':id/edit',
     component: EditCategoryComponent,
+    canActivate: [TitleGuard],
+    data: {
+      title: 'edit-category',
+    },
   },
 ];
 
--- frontend/src/app/pages/user-finances/user-category/user-category-routing.module.ts
+++ frontend/src/app/pages/user-finances/user-category/user-category-routing.module.ts
@@ -1,20 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditCategoryComponent, NewCategoryComponent } from 'src/app/components/category';
+import { TitleGuard } from 'src/app/shared';
 import { UserCategoryComponent } from './user-category.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UserCategoryComponent,
+    canActivate: [TitleGuard],
+    data: {
+      title: 'categories',
+    },
   },
   {
     path: 'new',
     component: NewCategoryComponent,
+    canActivate: [TitleGuard],
+    data: {
+      title: 'new-category',
+    },
   },
   {
     path: ':id/edit',
     component: EditCategoryComponent,
+    canActivate: [TitleGuard],
+    data: {
+      title: 'edit-category',
+    },
   },
 ];
 
