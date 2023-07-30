870cb28140c4f07eebc20897cb576dede1febd1d
Gabriel Alves <bielvipbh@hotmail.com>
2023-07-29 23:40:14 -0300

    check WS connection

--- frontend/src/app/@core/services/stomp.service.ts
+++ frontend/src/app/@core/services/stomp.service.ts
@@ -1,14 +1,21 @@
-import { RxStomp } from "@stomp/rx-stomp";
+import { RxStomp, RxStompState } from "@stomp/rx-stomp";
 import { Injectable, OnDestroy } from "@angular/core";
 import { Auth, user, User as FirebaseUser } from "@angular/fire/auth";
 import { lastValueFrom, Observable } from "rxjs";
 import { take } from "rxjs/operators";
+import { UntilDestroy, untilDestroyed } from "@ngneat/until-destroy";
+import { HotToastService } from "@ngneat/hot-toast";
+import { TranslocoService } from "@ngneat/transloco";
 
 @Injectable()
+@UntilDestroy()
 export class StompService extends RxStomp implements OnDestroy {
+  private readonly connectionErrorToastId = "connectionErrorToastId";
   private user$: Observable<FirebaseUser>;
+  private firstClosedState = true;
+  private serverOffline = false;
 
-  constructor(private auth: Auth) {
+  constructor(private auth: Auth, private toast: HotToastService, private translocoService: TranslocoService) {
     super();
     this.user$ = user(this.auth).pipe();
   }
@@ -26,6 +33,28 @@ export class StompService extends RxStomp implements OnDestroy {
         });
       })
     });
+
+    this.connectionState$.pipe(untilDestroyed(this)).subscribe(state => {
+      if (state == RxStompState.CLOSED) {
+        if (this.firstClosedState) {
+          this.firstClosedState = false;
+        } else {
+          this.serverOffline = true;
+          this.toast.error(this.translocoService.translate("server-offline"), {
+            id: this.connectionErrorToastId,
+            autoClose: false,
+            dismissible: false
+          });
+        }
+      } else if (state == RxStompState.OPEN) {
+        if (this.serverOffline) {
+          this.toast.close(this.connectionErrorToastId);
+          this.toast.success(this.translocoService.translate("connection-reestablished"));
+          this.serverOffline = false;
+        }
+      }
+    });
+
     this.activate();
   }
 
