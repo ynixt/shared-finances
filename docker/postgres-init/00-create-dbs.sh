0ee1bea9520c55df89a7189a6f69db46cde1ea59
Gabriel Alves <bielvipbh@hotmail.com>
2025-08-29 19:01:54 -0300

    feat: add pghero

--- docker/postgres-init/00-create-dbs.sh
+++ docker/postgres-init/00-create-dbs.sh
@@ -9,6 +9,20 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 
   CREATE DATABASE ${POSTGRES_AUTH_DB} OWNER ${POSTGRES_AUTH_USER};
   CREATE DATABASE ${POSTGRES_APP_DB}  OWNER ${POSTGRES_APP_USER};
+
+  CREATE USER ${POSTGRES_PGHERO_USER} WITH PASSWORD '${POSTGRES_PGHERO_PASSWORD}';
+  GRANT CONNECT ON DATABASE ${POSTGRES_AUTH_DB} TO pghero;
+  GRANT CONNECT ON DATABASE ${POSTGRES_APP_DB} TO pghero;
+  GRANT pg_monitor TO pghero;
 EOSQL
 
-echo ">>> created databases!"
+for DB in "$POSTGRES_AUTH_DB" "$POSTGRES_APP_DB"; do
+  if [ -n "$DB" ]; then
+    echo ">>> enabling pg_stat_statements on '$DB'…"
+    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$DB" <<-EOSQL
+      CREATE EXTENSION IF NOT EXISTS pg_stat_statements;
+EOSQL
+  fi
+done
+
+echo ">>> created roles, databases and enabled pg_stat_statements!"
