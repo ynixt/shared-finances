ddc24023c2d1408997c84528f4adb7343ee72980
Gabriel Alves <bielvipbh@hotmail.com>
2026-04-22 12:28:21 -0300

    Create V47__DropRedundantCoveredIndexes.sql

---
+++ backend/src/main/resources/db/migration/V47__DropRedundantCoveredIndexes.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS idx_fin_goal_ledger_goal;
+
+DROP INDEX IF EXISTS idx_financial_goal_target_goal_id;
+
+DROP INDEX IF EXISTS idx_recurrence_event_beneficiary_event;
+
+DROP INDEX IF EXISTS idx_wallet_event_beneficiary_event;
